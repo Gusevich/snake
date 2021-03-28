d78e999907dc546bd81fec56eed8836e7dbaa802
Gusevich <grachovas78@gmail.com>
2021-03-28 20:59:04 +0300

    a snake constructor is created and new methods are added

--- js/snake.js
+++ js/snake.js
@@ -70,3 +70,93 @@ Block.prototype.drawCircle = function (color) {
 Block.prototype.equal = function (otherBlock) {
   return this.col === otherBlock.col && this.row === otherBlock.row;
 };
+
+let Snake = function () {
+    this.segments = [
+        new Block(7, 5),
+        new Block(6, 5),
+        new Block(5, 5)
+    ];
+    this.direction = 'right';
+    this.nextDirection = 'left';
+};
+
+Snake.prototype.draw = function () {
+    for(let i = 0; i < this.segments.length; i++) {
+        this.segments[i].drawSquare('Blue');
+    }
+};
+
+Snake.prototype.move = function () {
+    let head = this.segments[0];
+    let newHead;
+
+    if(this.direction === 'right') {
+        newHead = new Block(head.col + 1, head.row);
+    } else if(this.direction === 'down') {
+        newHead = new Block(head.col, head.row + 1);
+    } else if(this.direction === 'left') {
+        newHead = new Block(head.col - 1, head.row);
+    } else if(this.direction === 'up'){
+        newHead = new Block(head.col, head.row = 1);
+    }
+    if(this.checkCollision(newHead)) {
+        gameOver();
+        return;
+    }
+
+    this.segments.unshift(newHead);
+
+    if(newHead.equal(apple.position)) {
+        score++;
+        apple.move();
+    } else {
+        this.segments.pop();
+    }
+};
+
+Snake.prototype.checkCollision = function () {
+    let leftCollision = (head.col === 0);
+    let topCollision = (head.row === 0);
+    let rightCollision = (head.col === widthInBlocks - 1);
+    let downCollision = (head.row === heightInBlocks - 1);
+
+    let wallCollision = leftCollision || topCollision ||
+        rightCollision || downCollision;
+
+    let selfCollision = false;
+    for(let i = 0; i < this.segments.length; i++) {
+        if(head.equal(this.segments[i])) {
+            selfCollision = true;
+        }
+    }
+    return wallCollision || selfCollision;
+};
+
+let directions = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+}
+
+${'body'}.keydown(function (event) {
+    let newDirection = directions[event.keyCode];
+    if(newDirection !== undefined) {
+        snake.setDirection(newDirection);
+    }
+});
+
+Snake.prototype.setDirection = function (newDirection) {
+  if(this.direction === 'up' && newDirection === 'down') {
+      return;
+  } else if(this.direction === 'right' && newDirection === 'left') {
+      return;
+  } else if(this.direction === 'down' && newDirection === 'up') {
+      return;
+  } else if(this.direction === 'left' && newDirection === 'right') {
+      return;
+  }
+  this.nextDirection = newDirection;
+};
+
