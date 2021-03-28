0b88ad30fba6e06bfce7d948f61cc6fdea6386fc
Gusevich <grachovas78@gmail.com>
2021-03-28 15:58:59 +0300

    Main structure of the Snake project is ready.

---
+++ js/snake.js
@@ -0,0 +1,46 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const width = canvas.width;
+const height = canvas.height;
+
+
+const blockSize = 10;
+const widthInBlocks = width / blockSize;
+const heightInBlocks = height / blockSize;
+
+let score = 0;
+
+function drawScore() {
+    ctx.font = "20px Courier";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "left";
+    ctx.fillStyle = "Black";
+    ctx.fillText("Score: " + score, blockSize, blockSize);
+}
+drawScore();
+
+setInterval(() => {
+    ctx.clearRect(10, 10, 380, 15);
+    drawScore();
+    return score++;
+}, 100);
+
+function drawBorder() {
+    ctx.fillStyle = 'Gray';
+    ctx.fillRect(0, 0, width, blockSize);
+    ctx.fillRect(0, height - blockSize, width, blockSize);
+    ctx.fillRect(0, 0, blockSize, height);
+    ctx.fillRect(width - blockSize, 0, blockSize, height);
+
+}
+drawBorder();
+
+function gameOver() {
+    // clearInterval(intervalid);
+    ctx.font = "60px Courier";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Game over", width / 2, height / 2);
+}
+gameOver();
